d1e959655d3fbc2b96f7c7068015c4ccf876fc7b
ND <hckray@vladloh.com>
2019-09-28 18:44:00 +0300

    lel

---
+++ src/clean_spiders/tb.py
@@ -0,0 +1,15 @@
+import telebot
+from telebot import types
+from telebot import apihelper
+from sconfig import telegram_token
+
+
+
+
+
+
+
+apihelper.proxy = {'https':'socks5://sox.ctf.su:1080', 'http' : 'socks5://sox.ctf.su:1080'}
+bot = telebot.TeleBot(telegram_token)
+
+bot.send_message(841622311, 'hi')
