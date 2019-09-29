366a869ba2aa411cb2f35ff06bf5f32ec8338824
ND <hckray@vladloh.com>
2019-09-29 07:02:42 +0300

    a

--- src/clean_spiders/sconfig.py
+++ src/clean_spiders/sconfig.py
@@ -1,6 +1,6 @@
 our = "ccacc_ount"
 delay = 3600
 last_posts = 'src/clean_spiders/lp.db'
-vk_token = '01af95d701af95d701af95d7b801c28961001af01af95d75c21f075f7f18db53cdec995'
+vk_token = '7d4db69b7d4db69b7d4db69b3b7d20975077d4d7d4db69b20c24dc6bb4749a5327dbabb'
 size = 100
 telegram_token = "971766312:AAGKyqnw48n7gVPbABRKtGJfzEFe4-fZV3M"
