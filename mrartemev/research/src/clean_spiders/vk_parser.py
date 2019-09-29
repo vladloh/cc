f804f02065dabddc3d0f22a197f6c9aa735885be
ND <hckray@vladloh.com>
2019-09-29 09:02:38 +0300

    aa

--- mrartemev/research/src/clean_spiders/vk_parser.py
+++ mrartemev/research/src/clean_spiders/vk_parser.py
@@ -11,10 +11,10 @@ def get_last_vk(id, count = 5):
         try:
             nw = {}
             owner_id = i['owner_id']
-            post_id = id
+            post_id = i['id']
             nw['time'] = i['date']
             nw['text'] = i['text']
-            nw['id'] = post_id
+            nw['id'] = owner_id
             nw['network'] = 'vk'
             nw['url'] = f'https://vk.com/wall{owner_id}_{post_id}'
             data.append(nw)
