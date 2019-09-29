69c56a3174e7a1af79957091639acddc756559be
ND <hckray@vladloh.com>
2019-09-29 06:57:14 +0300

    azs

--- src/clean_spiders/tb.py
+++ src/clean_spiders/tb.py
@@ -3,7 +3,7 @@ from sconfig import telegram_token
 
 
 bot = telebot.TeleBot(telegram_token)
-@bot.message_handler(commands = [start])
+@bot.message_handler(commands = ['start'])
 def add_user(message):
     bot.send_message(message.chat.id, 'hi')
 
