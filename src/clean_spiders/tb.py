37823b1b171b2c909e62b703a50ab77a87b1dab1
ND <hckray@vladloh.com>
2019-09-29 06:56:09 +0300

    azs

--- src/clean_spiders/tb.py
+++ src/clean_spiders/tb.py
@@ -1,10 +1,13 @@
 import telebot
 from sconfig import telegram_token
-bot = telebot.TeleBot(telegram_token)
 
+
+bot = telebot.TeleBot(telegram_token)
+@bot.message_handler(commands = [start])
+def add_user(message):
+    bot.send_message(message.chat.id, 'hi')
 
 
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
-    print('kek')
