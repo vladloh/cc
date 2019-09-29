2146eb1d4f7e1eed64200f8769a1c7e9ba71e5c1
ND <hckray@vladloh.com>
2019-09-29 11:04:34 +0300

    a

--- mrartemev/research/tb.py
+++ mrartemev/research/tb.py
@@ -1,4 +1,5 @@
 import telebot
+import similarity as sim
 from sconfig import telegram_token
 from dbworker import insert_user, get_all_users, get_all_posts
 
@@ -14,13 +15,17 @@ def add_user(message):
         insert_user(message.chat.id)
 
 
-@bot.message_handler(commands = ['new'])
-#@bot.callback_query_handler(func = lambda call: True and call.data.startswith('new')
-def get_post(message):
-    res = get_all_posts()
-    print(res)
-    kek = res[0]
-    bot.send_message(message.chat.id, kek['url'])
+# @bot.message_handler(commands = ['new'])
+# #@bot.callback_query_handler(func = lambda call: True and call.data.startswith('new')
+# def get_post(message):
+#     res = get_all_posts()
+#     print(res)
+#     kek = res[0]
+#     bot.send_message(message.chat.id, kek['url'])
 
-if __name__ == "__main__":
-    bot.polling(none_stop=True)
+# if __name__ == "__main__":
+#     bot.polling(none_stop=True)
+
+@bot.message_handler(type = 'text')
+def reply(message):
+    bot.send_message(message.chat.id, 'kek')
