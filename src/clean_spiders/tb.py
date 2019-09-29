89e80992f447762242723f1f24be4cdfce2aa160
ND <hckray@vladloh.com>
2019-09-29 07:09:53 +0300

    a

--- src/clean_spiders/tb.py
+++ src/clean_spiders/tb.py
@@ -1,11 +1,13 @@
 import telebot
 from sconfig import telegram_token
-
+from dbworker import reset_table, insert 
 
 bot = telebot.TeleBot(telegram_token)
 @bot.message_handler(commands = ['start'])
 def add_user(message):
+    reset_table()
     bot.send_message(message.chat.id, 'hi')
+    insert(message.chat.id)
 
 
 
