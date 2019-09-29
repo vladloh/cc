c7eb1fb816db34c8353875b147ff97b577e58278
ND <hckray@vladloh.com>
2019-09-29 07:25:26 +0300

    aa

--- src/clean_spiders/dbworker.py
+++ src/clean_spiders/dbworker.py
@@ -1,4 +1,5 @@
 from vedis import Vedis
+
 import sconfig
 
 
@@ -28,3 +29,72 @@ def set_state(user_id, value, social_network = 'inst'):
             print('Проснись, ты обосрался!') # Помянем Санька 
             #print(key, str(value))
             return False
+
+import sqlite3
+
+
+TABLE_NAME = "mem_table"
+
+
+def command(func):
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('src/clean_spiders/my.db')
+        try:
+            res = func(conn.cursor(), *args, **kwargs)
+        finally:
+            conn.commit()
+            conn.close()
+        return res
+
+    return wrapper
+
+
+@command
+def get_all(cursor):
+    cursor.execute('''
+        SELECT * FROM {};
+        '''.format(TABLE_NAME))
+    records = cursor.fetchall()
+    return records
+
+
+@command
+def reset_table(cursor):
+    cursor.execute('''
+        DROP TABLE IF EXISTS {};
+        '''.format(TABLE_NAME))
+    cursor.execute('''
+        CREATE TABLE {} (
+        ID INTEGER NOT NULL PRIMARY KEY,
+        TELEGRAM_ID INTEGER NOT NULL );
+                   '''.format(TABLE_NAME))
+
+
+@command
+def insert(cursor, uid):
+    cursor.execute('''
+        INSERT INTO {} (TELEGRAM_ID)
+        VALUES (?);
+    '''.format(TABLE_NAME, ), (uid, ))
+
+
+@command
+def delete(cursor, uid):
+    cursor.execute('''
+        DELETE FROM {} WHERE TELEGRAM_ID = ?;
+    '''.format(TABLE_NAME, ), (uid, ))
+
+
+@command
+def delete_all(cursor):
+    cursor.execute('''
+            DELETE FROM {};
+            '''.format(TABLE_NAME))
+
+def insert_user(telegram_id):
+    insert(telegram_id)
+
+def get_all_users():
+    return list(set([j for i, j in get_all()]))
+
+
--- src/clean_spiders/tb.py
+++ src/clean_spiders/tb.py
@@ -1,13 +1,12 @@
 import telebot
 from sconfig import telegram_token
-from dbworker import reset_table, insert 
+from dbworker import insert_user
 
 bot = telebot.TeleBot(telegram_token)
 @bot.message_handler(commands = ['start'])
 def add_user(message):
-    reset_table()
     bot.send_message(message.chat.id, 'hi')
-    insert(message.chat.id)
+    insert_user(message.chat.id)
 
 
 
