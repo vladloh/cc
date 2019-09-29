f21532a6fa02ee52389aa364ed96b96a7c8b5324
ND <hckray@vladloh.com>
2019-09-29 06:13:24 +0300

    azs

--- src/clean_spiders/tb.py
+++ src/clean_spiders/tb.py
@@ -1,13 +1,10 @@
 import telebot
 from sconfig import telegram_token
-
-
-
-
-
-
-
-#apihelper.proxy = {'https':'socks5://sox.ctf.su:1080', 'http' : 'socks5://sox.ctf.su:1080'}
 bot = telebot.TeleBot(telegram_token)
 
-bot.send_message(841622311, 'hi')
+
+
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
+    print('kek')
